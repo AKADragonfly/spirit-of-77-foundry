931ba376034bd374172bea5c193e806c3f6624da
AKADragonfly <TGIDragonfly@gmail.com>
2025-08-04 23:06:32 -0400

    Update spirit77.mjs

--- module/spirit77.mjs
+++ module/spirit77.mjs
@@ -46,7 +46,7 @@ async function createItemMacro(data, slot) {
 
   // Create the macro command
   const command = `game.spirit77.rollItemMacro("${item.name}");`;
-  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
   if (!macro) {
     macro = await Macro.create({
       name: item.name,
